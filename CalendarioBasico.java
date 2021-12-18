5218f1cba9f76a421804b1784ac6bd9700c82dcc
youssef <youssefleon75rgmail.com>
2021-12-18 13:20:05 +0100

    01 Los atributos y el constructor

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -1,14 +1,14 @@
 
 /**
- * Write a description of class CalendarioBasico here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Youssef) 
+ * @version (18-12-2021)
  */
 public class CalendarioBasico
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private int dia;
+    private int mes;
+    private int ano;
 
     /**
      * Constructor for objects of class CalendarioBasico
@@ -16,18 +16,8 @@ public class CalendarioBasico
     public CalendarioBasico()
     {
         // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        dia = 1;
+        mes = 1;
+        ano = 1;
     }
 }
