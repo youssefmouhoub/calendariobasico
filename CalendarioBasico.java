c5be4f4b82fa1a954195c1706ec5ead024c2c330
youssef <youssefleon75rgmail.com>
2021-12-21 11:16:38 +0100

    03 mejorando el calendario

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -20,4 +20,36 @@ public class CalendarioBasico
         mes = 1;
         ano = 1;
     }
+    
+    public String obtenerFecha() {
+        String fecha;
+        fecha = (dia + "-" + mes + "-" + ano);
+        return fecha;
+    }
+    
+    public void fijarFecha(int nuevoDia, int nuevoMes, int nuevoAno) {
+        dia = nuevoDia;
+        mes = nuevoMes;
+        ano = nuevoAno;
+    }
+    
+    public void avanzarFecha () {
+        if (dia <= 30) {
+            dia = dia + 1;
+            if (dia == 31) {
+                dia = 1;
+                mes = mes + 1;
+                if (mes == 13) {
+                    dia = 1;
+                    mes = 1;
+                    ano = ano + 1;
+                    if (ano == 100) {
+                        dia = 1;
+                        mes = 1;
+                        ano = 1;
+                    }
+                }
+            }
+        }
+    }
 }
